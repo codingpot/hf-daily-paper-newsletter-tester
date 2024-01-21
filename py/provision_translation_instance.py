d2992cf1a6f8f05d2244d6afeeb324c8809ba211
Chansung Park <deep.diver.csp@gmail.com>
2024-01-21 13:30:36 +0900

    Update provision_translation_instance.py

--- py/provision_translation_instance.py
+++ py/provision_translation_instance.py
@@ -6,7 +6,7 @@ import concurrent.futures
 
 from dstack.api import Client, Task, GPU, Client, Resources
 
-def parallel_job(i, client, arxiv_id, args):
+def provisioning_job(i, client, arxiv_id, args):
   port = 6006 + i
   
   task = Task(
@@ -65,29 +65,27 @@ def parallel_job(i, client, arxiv_id, args):
       ),
       spot_policy="on-demand",
      retry_policy={"retry":True, "limit":3}
-#      retry_policy=RetryPolicy(
-#        retry=True, limit=3
-#      )
   )
+  return run
 
-  interval = 10
-  attached = False
-  attached = run.attach()
-  print(f"0. attached = {attached}")
+  # interval = 10
+  # attached = False
+  # attached = run.attach()
+  # print(f"0. attached = {attached}")
   
-  while run.is_finished():
-    if not attached:
-      attached = run.attach()
+  # while run.is_finished():
+  #   if not attached:
+  #     attached = run.attach()
 
-    print(f"1. attached = {attached}")
+  #   print(f"1. attached = {attached}")
     
-    time.sleep(interval)
-    run.detach()
-    attached = False
+  #   time.sleep(interval)
+  #   run.detach()
+  #   attached = False
 
-    print(f"2. attached = {attached}")
+  #   print(f"2. attached = {attached}")
 
-  return f'{args.dstack_run_name}({arxiv_id}) is completed'
+  # return f'{args.dstack_run_name}({arxiv_id}) is completed'
 
 def main(args):
   client = Client.from_config(
@@ -97,17 +95,63 @@ def main(args):
   )
   
   num_jobs = len(args.arxiv_ids)
+
+  runs = []
   
-  clients = [client for _ in range(num_jobs)]
   arxiv_ids = [arxiv_id for arxiv_id in args.arxiv_ids]
   args_list = [args for _ in range(num_jobs)]
-  
-  with concurrent.futures.ThreadPoolExecutor(max_workers=num_jobs) as executor:
-    futures = [
-      executor.submit(parallel_job, i, clients[i], arxiv_ids[i], args_list[i]) for i in range(num_jobs)
-    ]
+
+  for i, (arxiv_id, args_item) in enumerate(zip(arxiv_ids, args_list)):
+    run = provisioning_job(
+      i, client, arxiv_id, args_item
+    )
+    runs.append(run)
+
+  attached_list = [False for _ in range(len(runs))]
+  finished_list = [False for _ in range(len(runs))]
+  all_finished = False
+  interval = 5
+
+  while not all_finished:
+    count = len(finished_list)
+    for i in range(len(runs)):
+      if not finished_list[i]:
+        run = runs[i]
+
+        try:
+          run.attach()
+          if run.is_finished():
+            finished_list[i] = True
+          run.detach()
+        except:
+          finished_list[i] = True
+        
+        count = count - 1
+
+    if count == 0:
+      all_finished = True
+    else:
+      all_finished = False
     
-    concurrent.futures.wait(futures)
+    time.sleep(interval)
+
+  for i in range(len(runs)):
+    if not finished_list[i]:
+      
+  
+  for finished in finished_list:
+    if not finished
+  
+  for run in runs:
+    run.attach()
+    
+
+  # with concurrent.futures.ThreadPoolExecutor(max_workers=num_jobs) as executor:
+  #   futures = [
+  #     executor.submit(parallel_job, i, clients[i], arxiv_ids[i], args_list[i]) for i in range(num_jobs)
+  #   ]
+    
+  #   concurrent.futures.wait(futures)
 
   # last_idx = num_jobs-1
   # parallel_job(last_idx+1, clients[last_idx], arxiv_ids[last_idx], args_list[last_idx])
